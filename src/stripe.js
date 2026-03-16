39aed18e24373769fcc7dcd15aae21dd5556d077
easysolutions906 <easysolutions906@users.noreply.github.com>
2026-03-16 12:19:32 -0400

    Add real Stripe prices and npm funding

--- src/stripe.js
+++ src/stripe.js
@@ -2,10 +2,8 @@ import Stripe from 'stripe';
 import { createKey, revokeKey, loadKeys } from './keys.js';
 
 const PRICE_TO_PLAN = {
-  'price_PLACEHOLDER_starter': 'starter',
-  'price_PLACEHOLDER_pro': 'pro',
-  'price_PLACEHOLDER_business': 'business',
-  'price_PLACEHOLDER_enterprise': 'enterprise',
+  'price_1TBdpS2dvwjzjXFJXWtjJgYw': 'starter',
+  'price_1TBdpT2dvwjzjXFJMeXtlny1': 'pro',
 };
 
 const PLAN_TO_PRICE = Object.fromEntries(
